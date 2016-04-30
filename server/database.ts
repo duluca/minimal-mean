8fe94523c29b8d1f40d9bfa116f76caf4497c352
Doguhan Uluca <duluca@gmail.com>
2016-04-30 11:28:59 -0400

    added async goodness

--- server/database.ts
+++ server/database.ts
@@ -3,7 +3,22 @@ import config = require('./config')
 
 const MongoClient = mongodb.MongoClient
 
+import bluebird = require('bluebird')
+const Collection = mongodb.Collection
+
+bluebird.promisifyAll(Collection.prototype)
+bluebird.promisifyAll(MongoClient)
+
 var dbInstance = {}
+
+export async function connectAsync() {
+  try {
+    dbInstance = await MongoClient.connectAsync(config.mongoUri)
+  } catch (e) {
+    console.log(e)
+    throw e
+  }
+}
 
 export function connect() {
   var database = this
--- server/index.ts
+++ server/index.ts
@@ -2,11 +2,13 @@ import * as config from './config'
 import * as server from './server'
 import * as database from './database'
 
-database.connect()
-  .then(function() {
-    console.log('Connected to database...')
+async function start() {
+  await database.connectAsync()
+  console.log('Connected to database asyncly...')
 
-    server.Instance.listen(config.port, () => {
-      console.log(`Server listening on port ${config.port}...`)
-    })
+  server.Instance.listen(config.port, () => {
+    console.log(`Server listening on port ${config.port}...`)
   })
+}
+
+start()
