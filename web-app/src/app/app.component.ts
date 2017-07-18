efebb46426f8f0794fc58a1d39dfd6023922e811
Doguhan Uluca <duluca@gmail.com>
2017-07-18 10:59:07 -0400

    Update to docker-compose

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -3,6 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  template: `
+    <div>
+      <h1>Hello</h1>
+    </div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
