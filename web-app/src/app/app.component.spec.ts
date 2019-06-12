672583a852001ac4a3a24322b754b5ff1a5856d6
Doguhan Uluca <duluca@gmail.com>
2019-06-12 01:42:22 -0400

    updated angular, fixed tests

--- web-app/src/app/app.component.spec.ts
+++ web-app/src/app/app.component.spec.ts
@@ -1,13 +1,14 @@
 import { TestBed, async } from '@angular/core/testing'
 
 import { AppComponent } from './app.component'
+import { MaterialModule } from './material.module'
+import { RouterTestingModule } from '@angular/router/testing'
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
+      imports: [MaterialModule, RouterTestingModule],
     }).compileComponents()
   }))
 
@@ -15,18 +16,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent)
     const app = fixture.debugElement.componentInstance
     expect(app).toBeTruthy()
-  }))
-
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent)
-    const app = fixture.debugElement.componentInstance
-    expect(app.title).toEqual('app works!')
-  }))
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent)
-    fixture.detectChanges()
-    const compiled = fixture.debugElement.nativeElement
-    expect(compiled.querySelector('h1').textContent).toContain('app works!')
   }))
 })
--- web-app/src/app/users/users.component.ts
+++ web-app/src/app/users/users.component.ts
@@ -7,11 +7,14 @@ import { Observable } from 'rxjs'
   selector: 'app-users',
   template: `
     <h2>All Users</h2>
+    <div *ngIf="users$ | async as users">
+    <div>Found {{users.total}} user(s)</div>
     <mat-list>
-      <mat-list-item *ngFor="let user of users$ | async">
+      <mat-list-item *ngFor="let user of users.data">
         <span>{{user.fullName}}</span>
       </mat-list-item>
     </mat-list>
+    </div>
   `,
 })
 export class UsersComponent {
